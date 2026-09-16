795eb48eb0e13ebab2c0c29626c3becb8da345b1
Navratan8949 <navratan0443@gmail.com>
2026-09-16 16:13:24 +0530

    change mail id again

--- backend/src/models/SiteContent.js
+++ backend/src/models/SiteContent.js
@@ -12,7 +12,7 @@ const siteContentSchema = new mongoose.Schema(
             url: { type: String, default: "" }
         },
         tagline: { type: String, default: "Build Digital Solutions That Move Your Business Forward" },
-        email: { type: String, default: "zdctechglobalsolutions@gmail.com" },
+        email: { type: String, default: "info@zdctechglobalsolutions.com" },
         phone: { type: String, default: "+91 7073551862" },
         headOffice: { type: String, default: "Jaipur, Rajasthan, India" },
         branchOffice: { type: String, default: "D. P. Road, Near New Nagar Parishad Office, Deulgaon Raja, Tq. Deulgaon Raja, Dist. Buldhana, Maharashtra - 443204" },
